8adb933e49692c3f1acbd1ac096f50a55cefa2e2
upcodestore <142344139+upcodestore@users.noreply.github.com>
2023-08-16 14:11:14 +0200

    Update config.lua
    clothing ID for male and female

--- UPCODE-CLOTHING/shared/config.lua
+++ UPCODE-CLOTHING/shared/config.lua
@@ -16,7 +16,7 @@ Config.MaxHeight = 0.5
 
 Config.Clothing = {
     mask = {
-        default = 0,
+        default = {male = 0, female = 0},
         animation = { dict = 'mp_masks@standard_car@ds@', name = 'put_on_mask', move = 51, duration = 800 }
     },
     hat = {
@@ -26,19 +26,19 @@ Config.Clothing = {
         animation = { dict = 'clothingspecs', name = 'take_off', move = 51, duration = 1400 }
     },
     shirt = {
-        default = 15,
+        default = {male = 15, female = 15},
         animation = { dict = 'missmic4', name = 'michael_tux_fidget', move = 51, duration = 1500 }
     },
     undershirt = {
-        default = 15,
+        default = {male = 15, female = 15},
         animation = { dict = 'missmic4', name = 'michael_tux_fidget', move = 51, duration = 1500 }
     },
     vest = {
-        default = 0,
+        default = {male = 0, female = 0},
         animation = { dict = 'clothingtie', name = 'try_tie_negative_a', move = 51, duration = 1200 }
     },
     chain = {
-        default = 0,
+        default = {male = 0, female = 0},
         animation = { dict = 'clothingtie', name = 'try_tie_positive_a', move = 51, duration = 2100 }
     },
     watch = {
@@ -48,15 +48,15 @@ Config.Clothing = {
         animation = { dict = 'nmt_3_rcm-10', name = 'cs_nigel_dual-10', move = 51, duration = 1200 }
     },
     pants = {
-        default = 14,
+        default = {male = 14, female = 14},
         animation = { dict = 're@construction', name = 'out_of_breath', move = 51, duration = 1300 }
     },
     bag = {
-        default = 0,
+        default = {male = 0, female = 0},
         animation = { dict = 'clothingtie', name = 'try_tie_negative_a', move = 51, duration = 1200 }
     },
     shoes = {
-        default = 34,
+        default = {male = 34, female = 34},
         animation = { dict = 'random@domestic', name = 'pickup_low', move = 0, duration = 1200 }
     }
 }
